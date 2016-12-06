514429f2b3f342d75c9dea4ff974431ca8c780a4
Avekube <0xlaserlogic@gmail.com>
2016-12-05 20:38:53 -0600

    Update

---
+++ Alien Pet RPG/Assets/Scripts/Inspectable.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Inspectable : MonoBehaviour {
+
+    // Use this for initialization
+    string inspectionMessage;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
